c5b7617f4a8b1aa5890c4a2ac4657f22c9251039
Ahmed-Magdy-12 <mhmedshalaby1234@gmail.com>
2022-11-11 20:10:46 +0200

    first commit

---
+++ widget.cpp
@@ -0,0 +1,32 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include "QUdpSocket"
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+    mSocket = new QUdpSocket(this);
+
+    connect(mSocket, &QUdpSocket::readyRead, [&](){
+      if( mSocket->hasPendingDatagrams()){
+            QByteArray datagram;
+            datagram.resize(mSocket->pendingDatagramSize());
+            mSocket->readDatagram(datagram.data(), datagram.size());
+            ui->listWidget->addItem(QString(datagram));
+         }
+    });
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+void Widget::on_connect_clicked()
+{
+    mSocket->bind(ui->port->value(), QUdpSocket::ShareAddress);
+}
+
